6fc57bf3b167fdc131995aec713027ac712f3f18
unknown <baomakb24@gmail.com>
2022-12-12 21:43:59 -0500

    Lab-8
    K to lose lives
    P to pause

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 [DefaultExecutionOrder(-1)]
 public class GameManager : MonoBehaviour
 {
     static GameManager _instance = null;
+
 
     public static GameManager instance
     {
@@ -37,11 +39,12 @@ public class GameManager : MonoBehaviour
             if (_lives < 0)
                 GameOver();
 
+            onLifeValueChanged?.Invoke(_lives);
             Debug.Log("Lives have been set to: " + _lives.ToString());
         }
     }
 
-
+    [HideInInspector] public UnityEvent<int> onLifeValueChanged;
 
     // Start is called before the first frame update
     void Awake()
@@ -61,20 +64,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (SceneManager.GetActiveScene().name == "Gameover")
-            {
-                SceneManager.LoadScene("Title");
-                playerInstance = null;
-            }
-            else
-                SceneManager.LoadScene("Level");
+       if (Input.GetKeyDown(KeyCode.Escape))
+       {
+          if (SceneManager.GetActiveScene().name == "Gameover")
+          {
+            SceneManager.LoadScene("Title");
+            playerInstance = null;
+          }
+        
         }
 
         if (Input.GetKeyDown(KeyCode.K))
             lives--;
-      
+       
+
     }
 
 
